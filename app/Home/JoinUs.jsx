4688d473f577ab5896838ddad71344b64ae8c200
Thanatos1204 <bhargavpandit01@gmail.com>
2023-12-23 18:13:05 +0530

    Conflict resolved again

--- app/Home/JoinUs.jsx
+++ app/Home/JoinUs.jsx
@@ -8,7 +8,7 @@ const JoinUs = () => {
         <main>
             <div className="join_us">
                 <div className="ca">
-                    <p className="font-bold mr-2"> <br /> <FaChalkboardTeacher />Join us and build web3 education's future</p>
+                    <p className="font-bold mr-2"> <br /> <FaChalkboardTeacher />Join us and build web3 education&apos;s future</p>
                     <br /> <p className="fill-form"><a href="https://h3sy8tusoyk.typeform.com/to/ZScdGa5l?typeform-source=www.theblockchainhive.com">Fill Campus ambassador form➔</a></p>
                 </div>
                 <div className="educator">
