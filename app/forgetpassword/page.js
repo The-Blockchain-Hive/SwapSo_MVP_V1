4572da7e62e204e2ad3b9cd4c9d475bb3c00d609
Ayush Pandey <aayushp9sm18@gmail.com>
2024-02-10 12:16:54 +0530

    error resolved

--- app/forgetpassword/page.js
+++ app/forgetpassword/page.js
@@ -10,7 +10,7 @@ const ForgetPassword = () => {
           {/* Page header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1 mb-4">Forgot your password?</h1>
-            <p className="text-xl text-gray-400">We'll email you instructions on how to reset it.</p>
+            <p className="text-xl text-gray-400">We&apos;ll email you instructions on how to reset it.</p>
           </div>
 
           {/* Form */}
