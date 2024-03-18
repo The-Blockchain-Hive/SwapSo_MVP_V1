15343c290c7310e3c76f1b0f9cc9101e0411fe5f
Mohammad Mudassir <mdmudassir0143@gmail.com>
2024-03-19 00:13:12 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { wagmiConfig, chains } from "./utils/wallet-utils";
 import DataContextProvider from "./context/DataContextProvider";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from '@vercel/analytics/react';
 
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Analytics />
       <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
       <AuthContextProvider> 
