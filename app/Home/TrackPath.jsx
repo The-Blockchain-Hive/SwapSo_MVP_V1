a00f62695708f1830f0380d09ba7029874544b42
Ayush Pandey <aayushp9sm18@gmail.com>
2024-03-25 01:47:19 +0530

    content updated

--- app/Home/TrackPath.jsx
+++ app/Home/TrackPath.jsx
@@ -10,12 +10,11 @@ const TrackPath = () => {
         {/* stack 1 */}
         <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE1</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center">Q1-2024</h1>
             <ul className="list-disc ml-4">
-              <li> Launching MVP</li>
-              <li>Simplifying payment</li>
-              <li>Gain initial 10,000 users</li>
-              <li>Implement Blockchain courses in Universities.</li>
+              <li>Initial Website Launch</li>
+              <li>Initial Course Onboarding</li>
+              <li>Swapso EdTech Launch</li>
             </ul>
           </div>
         </div>
@@ -32,11 +31,10 @@ const TrackPath = () => {
           </div>
           <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE 2</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center">Q2-2024</h1>
             <ul className="list-disc ml-4">
-              <li>Setting up an education based NGO</li>
-              <li>Educator KPIs and rating system</li>
-              <li>Smart contract based revenue sharing</li>
+              <li>Swapso Ecosystem Lite Paper</li>
+              <li>EdTech model expansion</li>
             </ul>
           </div>
         </div>
@@ -44,10 +42,11 @@ const TrackPath = () => {
           {/* stack3 */}
           <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE 3</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center"> Q3-2024</h1>
             <ul className="list-disc ml-4">
-              <li>Blockchain based certification</li>
-              <li>Onchain identity for students</li>
+              <li>Tokenomics announcement</li>
+              <li>Start Building on Layer 2 Blockchain</li>
+              <li>EdTech model expansion</li>
             </ul>
           </div>
         </div>
@@ -64,20 +63,22 @@ const TrackPath = () => {
           </div>
           <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE 4</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center"> Q4-2024</h1>
             <ul className="list-disc ml-4">
-              <li>Onboarding Industry partners to hire top talent. </li>
-              <li>Onchain Scholarships for top students. </li>
+              <li>Testnet Launch</li>
+              <li>University Partnerships</li>
             </ul>
           </div>
         </div>
           {/* stack5 */}
           <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE 5</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center">Q1-2025</h1>
             <ul className="list-disc ml-4">
-              <li>Open learning protocol</li>
-              <li>Accountability protocol</li>
+              <li>Token Public Launch</li>
+              <li>Staking pool announcement</li>
+              <li>Scholarship pool announcement</li>
+              <li>Bridge and Multiple Dex Listing</li>
             </ul>
           </div>
         </div>
@@ -94,9 +95,41 @@ const TrackPath = () => {
           </div>
           <div className="col-span-4 w-full h-full">
           <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
-            <h1 className="text-white text-xl font-bold py-2 text-center"> STAGE 6</h1>
+            <h1 className="text-white text-xl font-bold py-2 text-center"> Q2-2025</h1>
             <ul className="list-disc ml-4">
-              <li>We will Keep building the future</li>
+              <li>Mainnet Launch</li>
+              <li>On-chain Certification</li>
+              <li>Centralized Exchange (CEX) Listing</li>
+            </ul>
+          </div>
+        </div>
+        
+          <div className="col-span-4 w-full h-full">
+          <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
+            <h1 className="text-white text-xl font-bold py-2 text-center"> Q3-2025</h1>
+            <ul className="list-disc ml-4">
+              <li>Launch Smart wallets</li>
+            </ul>
+          </div>
+        </div>
+        <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+            <div className="h-full w-1 bg-indigo-300"></div>
+            <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center">7</div>
+          </div>
+        <div className="col-span-4 w-full h-full"></div>
+
+        <div className="col-span-4 w-full h-full"></div>
+          <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+            <div className="h-full w-1 bg-indigo-300"></div>
+            <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center">8</div>
+          </div>
+        <div className="col-span-4 w-full h-full">
+          <div className="w-full h-full bg-gradient-to-b from-black to-blue-1100 rounded-xl p-2 md:pl-4">
+            <h1 className="text-white text-xl font-bold py-2 text-center"> Q4-2025</h1>
+            <ul className="list-disc ml-4">
+              <li>On-chain Univeristy Trials</li>
+              <li>S-pay Launch</li>
+              <li>Token Trading Launch</li>
             </ul>
           </div>
         </div>
