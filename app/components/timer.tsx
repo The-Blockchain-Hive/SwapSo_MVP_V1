82807364f58ac954c13feafd3c183ee2f6b3a3cb
Ayush Pandey <aayushp9sm18@gmail.com>
2024-03-25 15:37:23 +0530

    timer updated

--- app/components/timer.tsx
+++ app/components/timer.tsx
@@ -4,21 +4,42 @@ interface TimerProps {
   selectedTimeframe: string;
 }
 
-
-const Timer: React.FC<TimerProps> = ({ selectedTimeframe }) => {  const [timeLeft, setTimeLeft] = useState(0);
+const Timer: React.FC<TimerProps> = ({ selectedTimeframe }) => {
+  const [timeLeft, setTimeLeft] = useState(() => {
+    const storedTimeLeft = localStorage.getItem('timeLeft');
+    const currentTime = Date.now();
+    const storedTimestamp = localStorage.getItem('timestamp');
+    const elapsedTime = storedTimestamp ? currentTime - parseInt(storedTimestamp) : 0;
+    const remainingTime = storedTimeLeft ? Math.max(0, parseInt(storedTimeLeft) - Math.floor(elapsedTime / 1000)) : 0;
+    return remainingTime;
+  });
 
   useEffect(() => {
-    const daysInSelectedTimeframe = parseInt(selectedTimeframe)
+    const daysInSelectedTimeframe = parseInt(selectedTimeframe);
     const timeframeInSeconds = daysInSelectedTimeframe * 24 * 60 * 60;
-    setTimeLeft(timeframeInSeconds);
-    // console.log("selected timeframe:", selectedTimeframe);
-    // console.log("time in seconds", timeframeInSeconds);
+    setTimeLeft((prevTime) => {
+      const storedTimestamp = localStorage.getItem('timestamp');
+      const currentTime = Date.now();
+      const elapsedTime = storedTimestamp ? currentTime - parseInt(storedTimestamp) : 0;
+      const remainingTime = Math.max(0, prevTime - Math.floor(elapsedTime / 1000));
+      return remainingTime > timeframeInSeconds ? timeframeInSeconds : remainingTime;
+    });
+
     const countdown = setInterval(() => {
       setTimeLeft((prevTime) => (prevTime <= 0 ? 0 : prevTime - 1));
     }, 1000);
 
     return () => clearInterval(countdown);
   }, [selectedTimeframe]);
+
+  useEffect(() => {
+    const currentTime = Date.now();
+    localStorage.setItem('timestamp', currentTime.toString());
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('timeLeft', timeLeft.toString());
+  }, [timeLeft]);
 
   const days = Math.floor(timeLeft / (60 * 60 * 24));
   const hours = Math.floor(((timeLeft % (60 * 60 * 24)) / (60 * 60)));
@@ -27,7 +48,7 @@ const Timer: React.FC<TimerProps> = ({ selectedTimeframe }) => {  const [timeLef
 
   return (
     <div>
-      {days} D : {hours} H : {minutes} M : {seconds} S  remaining
+      {days} D : {hours} H : {minutes} M : {seconds} S remaining
     </div>
   );
 };
